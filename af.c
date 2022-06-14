9ca30b54643f57099a7a15589eec5bb691e760c8
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-06-14 01:53:19 +0100

    My demo files

---
+++ af.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+				
+	char *argv[] = {"./if", NULL};
+	{
+	printf("Error :( :)\n");
+	}
+        printf("Prints After party... :) :) :)\n");
+}	
+
---
+++ bf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("First program\n");
+	printf("First Program\n");
+	return (0);
+}
