4276f9a8aebd98a139ae4dec07143c61bc4c1c79
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-06-21 23:17:53 +0100

    library demo

---
+++ library_demo/lib_add.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+
+void add(int a, int b)
+
+{
+	printf("addition result %d\n", a+b);
+}
---
+++ library_demo/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+  int x=10;
+  int y=5;
+  add(x,y);
+  sub(x,y);
+  return(0);
+}
